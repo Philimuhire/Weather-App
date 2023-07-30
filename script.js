8f25012bf6ba9ba32c168a9d56378469fcb46fcc
Philimuhire <philimuhire@gmail.com>
2023-07-30 08:17:45 +0200

    Called displayWeatherInfo in fetchWeatherData function

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const apiKey = '08b23855cea54f0c8d054458233007';
 const apiUrl = 'https://api.weatherapi.com/v1/current.json';
 
 async function fetchWeatherData(location) {
-  try {
-    const response = await fetch(`${apiUrl}?key=${apiKey}&q=${encodeURIComponent(location)}`);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error('Error fetching weather data:', error);
+    try {
+      const response = await fetch(`${apiUrl}?key=${apiKey}&q=${encodeURIComponent(location)}`);
+      const data = await response.json();
+      const processedData = processWeatherData(data);
+      displayWeatherInfo(processedData);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
   }
-}
+  
 
 function processWeatherData(data) {
     return {
